071ceef5eea83b96b7f73a889800d2a87620af41
Agustin Kassis <webmaster@masize.com>
2023-10-16 23:51:56 -0300

    started link card draft

--- src/screens/LinkCardQRScreen.js
+++ src/screens/LinkCardQRScreen.js
@@ -1,25 +1,80 @@
 import {useNavigation} from '@react-navigation/core';
 import React, {useEffect} from 'react';
-import {Button, ScrollView, StyleSheet, Text} from 'react-native';
+import {
+  Button,
+  NativeEventEmitter,
+  NativeModules,
+  ScrollView,
+  StyleSheet,
+  Text,
+} from 'react-native';
 import Dialog from 'react-native-dialog';
 import {Card, Title} from 'react-native-paper';
+
+const LinkStatus = {
+  IDLE: 'idle',
+  SCANNING: 'scanning',
+  TAPPING: 'tapping',
+};
+
+const eventEmitter = new NativeEventEmitter();
+
 export default function LinkCardQRScreen({route}) {
+  // status
+  const [linkStatus, setLinkStatus] = React.useState(LinkStatus.IDLE);
   // get data from QR
   const {data} = route.params || {};
 
   // use navigation
   const navigation = useNavigation();
 
+  // On exit screen
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('blur', () => {
+      // Do something when the screen blurs
+      setLinkStatus(LinkStatus.IDLE);
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  // Return QR Data
   useEffect(() => {
     if (!data) {
       return;
     }
     alert(`HAY DATA: ${data}`);
+    setLinkStatus(LinkStatus.TAPPING);
   }, [data]);
 
-  const startScan = () => {
-    navigation.navigate('ScanScreen', {backScreen: 'Link QR Main'});
+  // On status change
+  useEffect(() => {
+    switch (linkStatus) {
+      case LinkStatus.SCANNING:
+        navigation.navigate('ScanScreen', {backScreen: 'Link QR Main'});
+        break;
+      case LinkStatus.TAPPING:
+        NativeModules.MyReactModule.setCardMode('read');
+
+        const readEventListener = eventEmitter.addListener(
+          'CardHasBeenRead',
+          onReadCard,
+        );
+
+        return () => {
+          return readEventListener.remove();
+        };
+      default:
+        break;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [linkStatus]);
+
+  const onReadCard = event => {
+    const {cardId} = event;
+    alert(`Card ID: ${cardId}`);
   };
+
   return (
     <ScrollView>
       <>
@@ -29,7 +84,10 @@ export default function LinkCardQRScreen({route}) {
             <Text>First scan QR Code</Text>
           </Card.Content>
           <Card.Actions style={styles.spaceAround}>
-            <Button onPress={startScan} title="Scan QR Code" />
+            <Button
+              onPress={() => setLinkStatus(LinkStatus.SCANNING)}
+              title="Scan QR Code"
+            />
           </Card.Actions>
         </Card>
         <Dialog.Container visible={false}>
