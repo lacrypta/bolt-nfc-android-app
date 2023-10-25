7a39cfc3cbd7cf302b0ec264dfe9d6802088e435
Agustin Kassis <webmaster@masize.com>
2023-10-25 14:57:53 -0300

    added text-encoding-polyfill package

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /**
  * @format
  */
-import './shim.js'
+import './shim.js';
 require('crypto');
 import 'react-native-reanimated';
+import 'text-encoding-polyfill';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
