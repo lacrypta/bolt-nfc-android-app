bd4ba7e7d04ef734098c093d2dc085064fcd5269
Agustin Kassis <webmaster@masize.com>
2023-10-25 19:48:23 -0300

    added createAsociateCardEvent

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -31,7 +31,24 @@ export function createInitializeCardEvent(cardUID, design, cardModulePubKey) {
   };
 }
 
+export function createAsociateCardEvent(otc, cardModulePubKey) {
+  const body = {
+    otc,
+  };
+
+  return {
+    kind: 21111,
+    created_at: Math.floor(Date.now() / 1000),
+    tags: [
+      ['p', cardModulePubKey],
+      ['t', 'card-associate-request'],
+    ],
+    content: JSON.stringify(body),
+  };
+}
+
 module.exports = {
   getQueryParam,
   createInitializeCardEvent,
+  createAsociateCardEvent,
 };
